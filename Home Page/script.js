2d38dfadfa5a8eb2906b777f21849361cb1b9a28
Catalin <geaigelsaflu@gmail.com>
2023-07-20 16:01:58 +0300

    Added name animation
    Name deletes and rewrites itself. Also worked on fonts.

---
+++ Home Page/script.js
@@ -0,0 +1,47 @@
+window.addEventListener('load', function () {
+    init();
+    this.window.setInterval(nameAnimate,100);
+});
+
+let divName;
+let spanName;
+let fullName = "Danis Catalin";
+
+function init(){
+    divName = document.getElementById("name");
+    spanName = divName.getElementsByTagName("span")[0];   
+}
+
+//deleting and rewriting the name from the header
+//between each time a letter is deleted or rewrited, 
+//it has a 150ms pause for a fluid move 
+//alos it has a 1.35s pause when name is full
+let deleting = false;
+var waited = 0;
+function nameAnimate(){
+        if(spanName.textContent.length == 1)
+            deleting = false;
+
+        if(!deleting && spanName.textContent.length == fullName.length){
+            deleting = true;
+            waited = 0;
+        }
+
+        if(!deleting){
+            spanName.textContent = fullName.substring(0,spanName.textContent.length+1);
+            if(spanName.textContent[spanName.textContent.length-1] == ' ')
+                spanName.textContent = fullName.substring(0,spanName.textContent.length+1);
+        }
+
+        if(deleting){
+            if(waited < 9){
+                waited++;
+            }
+            else{
+                spanName.textContent = fullName.substring(0,spanName.textContent.length-1);
+                if(spanName.textContent[spanName.textContent.length-1] == ' ')
+                    spanName.textContent = fullName.substring(0,spanName.textContent.length-1);
+            }
+        }     
+}
+
